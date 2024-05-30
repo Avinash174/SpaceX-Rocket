a4628ab50dbc7cbdd3ce8a8ef52858f95072f697
Avinash174 <avinashmagar15@gmail.com>
2024-05-30 13:41:10 +0530

    new changes done

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -10,10 +10,6 @@ class SpalshScreen extends StatefulWidget {
 class _SpalshScreenState extends State<SpalshScreen> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-       
-      ],
-    );
+    return Scaffold();
   }
 }
